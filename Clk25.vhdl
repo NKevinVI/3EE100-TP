d819b1165c5b477c3271acd7e435f1e7ab4fb5cc
NKevinVI <kevinnoche666@gmail.com>
2023-10-19 13:15:28 +0200

    Renommer Horloge25 en Clk25.

--- Clk25.vhdl
+++ Clk25.vhdl
@@ -4,16 +4,16 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.std_logic_arith.all;
 
 
-entity ClkDiv is
+entity Clk25 is
     port(Clk100: in std_logic;
         Reset: in std_logic;
-        Clk25: out std_logic);
+        Clk_25: out std_logic);
 end entity;
 
-architecture comport of ClkDiv is
+architecture comport of Clk25 is
     signal reg: std_logic_vector(1 downto 0);
     begin
-    Clk25 <= not(reg(1));
+    Clk_25 <= not(reg(1));
     process(CLk100, Reset)
         begin
         if (Reset = '0') then
